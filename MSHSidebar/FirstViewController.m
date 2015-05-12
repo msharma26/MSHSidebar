e48ea518dc9d337ba6c7179b15bb45ae10ece01e
Manu Sharma <manusharma.aug26@gmail.com>
2015-05-12 07:42:39 -0400

    Sidebar

--- MSHSidebar/FirstViewController.m
+++ MSHSidebar/FirstViewController.m
@@ -8,20 +8,96 @@
 
 #import "FirstViewController.h"
 
+#define kSnapThreshold 40
+
 @interface FirstViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *panningView;
 @end
 
 @implementation FirstViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIPanGestureRecognizer* pgr = [[UIPanGestureRecognizer alloc]
+                                   initWithTarget:self
+                                   action:@selector(handlePan:)];
+    [self.panningView addGestureRecognizer:pgr];
 }
+
+
+float oldX, oldY;
+BOOL dragging;
+
+-(void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    dragging = YES;
+    
+    UITouch* touch = [[event allTouches] anyObject];
+    CGPoint touchPoint = [touch locationInView:self.view];
+    
+        oldX = touchPoint.x;
+        oldY = touchPoint.y;
+
+}
+
+-(void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
+    UITouch* touch = [[event allTouches] anyObject];
+    CGPoint touchPoint = [touch locationInView:self.view];
+    CGRect movingFrame = self.view.frame;
+    //movingFrame.origin.x =
+}
+-(void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
+    dragging = NO;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+}
+
+
+-(void)handlePan:(UIPanGestureRecognizer*)pgr;
+{
+    if (pgr.state == UIGestureRecognizerStateChanged) {
+        CGPoint center = pgr.view.center;
+        CGPoint translation = [pgr translationInView:pgr.view];
+        center = CGPointMake(center.x + translation.x,
+                             center.y);
+        NSLog(@"%f", center.x);
+        pgr.view.center = center;
+        [pgr setTranslation:CGPointZero inView:pgr.view];
+        
+    } else if (pgr.state == UIGestureRecognizerStateEnded) {
+        [self snapView:pgr.view toView:self.view];
+
+
+
+    }
+}
+
+-(void) snapView: (UIView*)topView toView: (UIView*) baseView{
+
+    if (topView.center.x <= baseView.center.x+kSnapThreshold || topView.center.x <= baseView.center.x-kSnapThreshold){
+        [UIView animateWithDuration:0.2 animations:^{
+            topView.center = baseView.center;
+        } completion:^(BOOL finished) {
+            
+        }];
+
+    }
+    
+    else if (topView.center.x <=baseView.center.x*2 - kSnapThreshold || topView.center.x >=baseView.center.x*2 ) {
+        CGPoint center = CGPointMake( baseView.center.x*2 +kSnapThreshold, baseView.center.y);
+        [UIView animateWithDuration:0.2 animations:^{
+            topView.center = center;
+        } completion:^(BOOL finished) {
+            
+        }];
+
+    }
+    
 }
 
 @end
